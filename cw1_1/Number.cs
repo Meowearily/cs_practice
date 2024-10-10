2759faa9692a2e8364fe4f13f3b458b34361871d
Diana Kuklina <kuklinadiana12@gmail.com>
2024-10-11 00:01:48 +0300

    add first task

---
+++ cw1_1/Number.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Number
+{
+    public int num = Convert.ToInt32(Console.ReadLine());
+
+    public void Print()
+    {
+        Console.WriteLine($"Вы ввели число: {num}");
+    }
+
+    public bool IsThreeRank()
+    {
+        return !((num / 1000 > 0) || (num / 100 % 10 == 0));
+    }
+    public int Units()
+    {
+        return num % 10;
+    }
+
+    public int Tens()
+    {
+        return num / 10 % 10;
+    }
+
+    public int Hundreds()
+    {
+        return num / 100 % 10;
+    }
+
+    public int Sum()
+    {
+        return this.Units() + this.Tens() + this.Hundreds();
+    }
+
+    public int Product()
+    {
+        return this.Units() * this.Tens() * this.Hundreds();
+    }
+}
---
+++ cw1_1/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+Number entry_num = new Number();
+entry_num.Print();
+
+if (entry_num.IsThreeRank())
+{
+    Console.WriteLine($"Число единиц в введеном числе: {entry_num.Units()}");
+    Console.WriteLine($"Число десятков в введеном числе: {entry_num.Tens()}");
+    Console.WriteLine($"Сумма цифр в введеном числе: {entry_num.Sum()}");
+    Console.WriteLine($"Произведение цифр в введеном числе: {entry_num.Product()}");
+}
+else
+{
+    Console.WriteLine("Вы ввели не трехзначное число");
+}
