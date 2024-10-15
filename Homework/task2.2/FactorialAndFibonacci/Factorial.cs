9213a7f4c55f5c885353119f5b597f7ce785bccf
Diana Kuklina <kuklinadiana12@gmail.com>
2024-10-15 17:25:40 +0300

    rename task's number and add task 2.2

---
+++ Homework/task2.2/FactorialAndFibonacci/Factorial.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics;
+
+class Factorial
+{
+    System.Numerics.BigInteger num;
+    System.Numerics.BigInteger factorial = 1;
+    public Factorial(System.Numerics.BigInteger num)
+    {
+        this.num = num;
+    }
+
+    public System.Numerics.BigInteger FactorialRecursion(System.Numerics.BigInteger num)
+    {
+        if (num == 1) return 1;
+        return num * FactorialRecursion(num - 1);
+    }
+
+    public System.Numerics.BigInteger FactorialIteration(System.Numerics.BigInteger num)
+    {
+        for (System.Numerics.BigInteger i = 1; i <= num; i++)
+        {
+            factorial *= i;
+        }
+        return factorial;
+    }
+}
---
+++ Homework/task2.2/FactorialAndFibonacci/Fibonacci.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Fibonacci
+{
+    System.Numerics.BigInteger num;
+    System.Numerics.BigInteger f1=0, f2=1;
+    public Fibonacci(System.Numerics.BigInteger num)
+    {
+        this.num = num;
+    }
+
+    public System.Numerics.BigInteger FibonacciRecursion(System.Numerics.BigInteger num)
+    {
+        if (num == 0 || num == 1) return num;
+        return FibonacciRecursion(num - 1) + FibonacciRecursion(num - 2);
+    }
+
+    public System.Numerics.BigInteger FibonacciIteration(System.Numerics.BigInteger num)
+    {
+        for (System.Numerics.BigInteger i = 1; i < num; i++)
+        {
+            f2 = f2 + f1;
+            f1 = f2 - f1;
+        }
+
+        return f2;
+    }
+}
